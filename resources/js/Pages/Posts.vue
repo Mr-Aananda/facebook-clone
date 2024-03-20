0b6ce3e3b1f0672df0b901854f640dd8427b7a80
Mr Aananda <rmananda007@gmail.com>
2024-03-20 15:47:19 +0600

    first commit

---
+++ resources/js/Pages/Posts.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainNavLayout from '@/Layouts/MainNavLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Posts" />
+    <MainNavLayout>
+        MainNavLayout
+    </MainNavLayout>
+</template>
